5369d0daf7de3f47020e86f69497502dc5b47c26
Jinyoung Jang <jyjang@uengine.org>
2022-11-25 13:22:04 +0000

    setting jpa token store

--- delivery/src/main/java/labshopeventsourcing/DeliveryApplication.java
+++ delivery/src/main/java/labshopeventsourcing/DeliveryApplication.java
@@ -1,11 +1,14 @@
 package labshopeventsourcing;
 
 import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.eventhandling.tokenstore.jpa.JpaTokenStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -16,6 +19,13 @@ public class DeliveryApplication {
         applicationContext = SpringApplication.run(DeliveryApplication.class, args);
     }
 
+
+    @Bean
+    public TokenStore myTokenStore() {
+        return JpaTokenStore.builder()
+                            // …
+                            .build();
+    }
 
     @Autowired
     public void configure(EventProcessingConfigurer config) {
